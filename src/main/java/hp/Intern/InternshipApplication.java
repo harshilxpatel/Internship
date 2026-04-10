3d0f5aa68c88cf8e30986bbc6844080a1735a260
HARSHIL PATEl <harshilhpatel2003@gmail.com>
2026-04-10 20:35:51 +0530

    feat: initial Spring Boot project setup with PostgreSQL

---
+++ src/main/java/hp/Intern/InternshipApplication.java
@@ -0,0 +1,12 @@
+package hp.Intern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipApplication.class, args);
+		System.out.println("Hello");
+	}
+}
